ccd817be73f95753eff5dcd0d4955294b7b37f94
archmagece <archmagece@gmail.com>
2014-03-10 02:36:31 +0900

    decorator연습코드들

---
+++ bs_basicutils/test3.py
@@ -0,0 +1,15 @@
+__author__ = 'archmagece'
+
+
+def superman(param):
+    """document
+    aaa
+    bbb
+    """
+    print param
+
+
+print superman.__name__
+print superman.__doc__
+print superman.__dict__
+
