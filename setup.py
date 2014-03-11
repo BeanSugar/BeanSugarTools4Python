48daed58078843d0d283bd39249ee33eaae7b7f9
archmagece <archmagece@gmail.com>
2014-03-12 07:09:18 +0900

    github down...

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+__author__ = 'archmagece'
+
+from distutils.core import setup
+
+setup(name='bs_tools',
+      version='0.1',
+      description='BS Tools for Python',
+      author='archmagece',
+      author_email='archmagece@gmail.com',
+      url='http://www.beansugar.org/',
+      packages=['bs_tools'],
+      #package_dir={},
+     )
